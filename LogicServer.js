50d19f6f9a32dd4761af8b57c588bfcc8d634a72
Chinna-Anaparthi <123636616+Chinna-Anaparthi@users.noreply.github.com>
2023-10-17 14:57:01 +0530

    first commit

---
+++ LogicServer.js
@@ -0,0 +1,98 @@
+const RegularFieldWork = require("./DatabaseConnection/RegularFieldWork")
+
+const multer = require('multer');
+const upload = multer();
+
+const FieldworkPost = async (req, res) => {
+    try {
+        upload.any()(req, res, async (error) => {
+            if (error) {
+                console.error('Error uploading files:', error);
+                return res.status(500).json({ error: 'Error uploading files' });
+            }
+
+            const images = req.files.filter(file => file.fieldname === 'geoTaggedPhotos')
+                .map(file => file.buffer.toString('base64'));
+
+            const petrolBillImage = req.files.find(file => file.fieldname === 'uploadPetrolBill');
+
+            const {
+                employeeId,
+                employeeName,
+                dateOfVisit,
+                natureOfFieldWork,
+                placesOfVisit,
+                purposeOfVisit,
+                briefReport,
+                villageWiseKeyPersons,
+                contactNumbers,
+                vehicleNumber,
+                openingReading,
+                closingReading,
+                totalKmsTravelled,
+                status,
+            } = req.body;
+
+            const newRecord = new RegularFieldWork({
+                employeeId,
+                employeeName,
+                dateOfVisit,
+                natureOfFieldWork,
+                placesOfVisit,
+                purposeOfVisit,
+                briefReport,
+                villageWiseKeyPersons,
+                contactNumbers,
+                geoTaggedPhotos: images,
+                uploadPetrolBill: petrolBillImage ? petrolBillImage.buffer.toString('base64') : null,
+                vehicleNumber,
+                openingReading,
+                closingReading,
+                totalKmsTravelled,
+                status,
+            });
+
+            await newRecord.save();
+
+            res.status(200).json({ message: 'RegularFieldWork Data saved to MongoDB successfully.' });
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error inserting data into MongoDB' });
+    }
+};
+
+
+const FieldworkStatusUpdate = async (req, res) => {
+    try {
+        const { status } = req.body;
+        const { employeeId } = req.params;
+
+        const updatedRecord = await RegularFieldWork.findOneAndUpdate(
+            { employeeId: employeeId },
+            { $set: { status: status } },
+            { new: true }
+        );
+
+        if (!updatedRecord) {
+            res.status(404).json({ error: 'Record with the provided employeeId not found in the database.' });
+        } else {
+            res.status(200).json({ message: 'Status updated successfully.', updatedRecord });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error updating status in MongoDB' });
+    }
+}
+
+const FieldworkGet = async (req, res) => {
+    try {
+        const results = await RegularFieldWork.find();
+        res.status(200).json(results);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error fetching data from MongoDB' });
+    }
+}
+
+module.exports = { FieldworkGet, FieldworkStatusUpdate, FieldworkPost }
---
+++ ServerAPI.js
@@ -0,0 +1,36 @@
+const express = require('express')
+let app = express()
+const Server= require("./LogicServer")
+const mongoose = require("mongoose");
+var parser = require("body-parser");
+const dotenv = require("dotenv").config();
+app.use(parser.json());
+const cors = require('cors');
+app.use(cors());
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Manager_Account MongoDB Connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.post('/RegularFieldWork', (req, res) => {
+    Server.FieldworkPost(req, res, () => { });
+})
+
+app.get('/RegularFieldWorkRetrive', (req, res) => {
+    Server.FieldworkGet(req, res, () => { })
+})
+
+app.put('/RegularFieldWork-Status-Update/:employeeId', (req, res) => {
+  Server.FieldworkStatusUpdate(req, res, () => { });
+})
+const port = process.env.PORT;
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`)
+})
